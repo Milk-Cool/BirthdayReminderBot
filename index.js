85c5b8c18a81c9401e14a6f1061eab4e520c0506
Milk-Cool <milan-pl@mail.ru>
2023-08-27 12:22:24 +0300

    ...now with proper encoding!

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ let bdays = [];
 })();
 
 http.createServer((req, res) => {
-    res.writeHead(200);
+    res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8"
+    });
     res.end(JSON.stringify(bdays));
 }).listen(8080);
